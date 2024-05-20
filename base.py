1af33bd3f1f6b81856ee4015d2b5a5d920e7fc38
yongqingzs <c1106381878@126.com>
2024-05-20 11:28:28 +0800

    改写了base的实现，使其更符合抽象。

--- base.py
+++ base.py
@@ -1,15 +1,12 @@
+from abc import abstractmethod
 import numpy as np
+
 
 class Protocol:
     """
     仅保留通信拓扑，以更好地抽象出一致性协议的基本结构
     """
-    def __init__(self, x_init, A):
-        self.dt = 0.01
-        if isinstance(x_init, np.ndarray):
-            self.x_init = x_init
-        else:
-            self.x_init = np.array([1, 2, 3, 4])
+    def __init__(self, A):
         # 通信拓扑
         if isinstance(A, np.ndarray):
             self.A = A
@@ -22,35 +19,63 @@ class Protocol:
             raise ValueError("A must be a numpy array or None")
         self.D = np.diag(np.sum(self.A, axis=1))
         self.L = self.D - self.A
+
+
+class BaseSystem:
+    """
+    状态、通信拓扑、控制量为所有系统共有的属性
+    """
+    def __init__(self, A, x_init):
+        self.dt = 0.01
+        if isinstance(x_init, np.ndarray):
+            self.x_init = x_init
+        else:
+            self.x_init = np.array([1, 2, 3, 4])
+        self.pro = Protocol(A)
         # 控制
         self.x = self.x_init
-        self.v = np.zeros_like(self.x)
         self.u = np.zeros_like(self.x)
         # 存储，利用list
         self.x_array = list()
         self.u_array = list()
-        self.v_array = list()
         self.x_array.append(self.x.tolist())
 
-class FirstOrderSystem:
-    def __init__(self, x_init, A):
-        self.sys = Protocol(x_init, A)
-    
-    def update(self, u):
-        self.sys.u = u
-        self.sys.x = self.sys.x + self.sys.dt * self.sys.u
-        # 存储
-        self.sys.x_array.append(self.sys.x.tolist())
-        self.sys.u_array.append(self.sys.u.tolist())
+    @abstractmethod
+    def update(self):
+        pass
 
-class SecondOrderSystem:
-    def __init__(self, x_init, A):
-        self.sys = Protocol(x_init, A)
+    def base_store(self):
+        self.x_array.append(self.x.tolist())
+        self.u_array.append(self.u.tolist())
+        
+
+class FirstOrderSystem(BaseSystem):
+    def __init__(self, A, x_init):
+        super().__init__(A, x_init)
     
     def update(self, u):
-        self.sys.u = u
-        self.sys.x = self.sys.x + self.sys.dt * self.sys.v  # 必须在np下进行
-        self.sys.v = self.sys.v + self.sys.dt * self.sys.u
+        self.u = u
+        self.x = self.x + self.dt * self.u
         # 存储
-        self.sys.x_array.append(self.sys.x.tolist())
-        self.sys.u_array.append(self.sys.u.tolist())
+        self.base_store()
+
+
+class SecondOrderSystem(BaseSystem):
+    def __init__(self, A, x_init, v_init):
+        self.sys = Protocol(A, x_init)
+        if isinstance(v_init, np.ndarray):
+            self.v_init = v_init
+        else:
+            self.v_init = np.array([1, 2, 3, 4])
+        self.v = v_init
+        self.v_array = list()
+        self.v_array.append(self.v.tolist())
+    
+    def update(self, u):
+        self.u = u
+        self.x = self.x + self.dt * self.v  # 必须在np下进行
+        self.v = self.v + self.dt * self.u
+        # 存储
+        self.base_store()
+        self.v_array.append(self.v.tolist())
+        
--- first_order.py
+++ first_order.py
@@ -10,18 +10,18 @@ if __name__ == '__main__':
                   [1, 0, 1, 0, 1, 0],
                   [0, 0, 0, 1, 0, 1],
                   [1, 0, 1, 0, 1, 0]])
-    ms = FirstOrderSystem(x_init, A)
+    ms = FirstOrderSystem(A, x_init)
 
     # 仿真参数
     time = 10  # s
     gamma = 1
-    time_l = int(10 / ms.sys.dt)
+    time_l = int(10 / ms.dt)
     for i in range(time_l):
         # 基本一致性协议
-        u = -gamma * np.dot(ms.sys.L, ms.sys.x)
+        u = -gamma * np.dot(ms.pro.L, ms.x)
         ms.update(u)
-    # 将fo.sys.x_array的各列画出
-    x_array = np.array(ms.sys.x_array)
+    # 将x_array的各列画出
+    x_array = np.array(ms.x_array)
     for i in range(x_init.shape[0]):
         plt.plot(x_array[:, i])
     plt.show()
